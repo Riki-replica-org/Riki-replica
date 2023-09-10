4cc23a38aa41cfe2f92932c17cd1f24cf2db881d
Lulseged Admasu <lulsegedwork@gmail.com>
2023-09-10 12:50:58 -0400

    riki setup

---
+++ config.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+
+# Be sure to change these directory information 
+CONTENT_DIR = '/Users/admasul1/Desktop/NKU Classes/CSC 440/Riki/content'
+USER_DIR = '/Users/admasul1/Desktop/NKU Classes/CSC 440/Riki/user'
+
+NUMBER_OF_HISTORY = 5
+PRIVATE = False
